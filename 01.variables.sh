a8656d6488631912c06b434cb905945c0df2ae14
tejas k <tejaskgowda1012@gmail.com>
2026-01-31 08:40:29 +0530

    variables

--- 01.variables.sh
+++ 01.variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$person1:suresh
-$person2:ramesh
+$person1::suresh
+$person2::ramesh
 
 echo "$person1:hi $person2, how are you"
 echo "$person2:Hello $person1, iam good, what about you"
