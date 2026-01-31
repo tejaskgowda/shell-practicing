ac2394bd9b3c11d6b7bdc836c8db1541b31af288
tejas k <tejaskgowda1012@gmail.com>
2026-01-31 08:24:15 +0530

    i am doing variables practicing

---
+++ 01.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1:suresh
+person2:ramesh
+
+echo "$person1:hi $person2, how are you"
+echo "$person2:Hello $person1, iam good, what about you"
+echo "$person1:I am fine $person2. what are you learning"
+echo" $person2:i am learning shell scripting $person1"
