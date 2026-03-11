65df2c6a71939a481c7aabc4b3f660b81f7b9bc9
tejas k <tejaskgowda1012@gmail.com>
2026-03-11 22:41:22 +0530

    act

--- 08-time.sh
+++ 08-time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 START_TIME=$(date +%s)
-echo "the script excuited at $START_TIME"
+echo "the script excuited at: $START_TIME"
 sleep 10
 END_TIME=$(date +%s)
-TOTAL_TIME=$($END_TIME-$START_TIME)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "the script excuited at: $TOTAL_TIME"
