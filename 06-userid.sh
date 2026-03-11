0af5a1faf0cf6c1ef47e19adfc33d0466bd57463
tejas k <tejaskgowda1012@gmail.com>
2026-03-11 20:10:32 +0530

    act

--- 06-userid.sh
+++ 06-userid.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "please enter your username ::"
 read USER_NAME
-echo "your user name is $user_name"
+echo "your user name is $USER_NAME"
+echo "please enterthe passward ::"
+read PASS_WARD
+echo "your pass ward is $PASS_WARD"
